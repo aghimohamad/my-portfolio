26275770efc6c0269c8916d380b4fd85beb4218f
mohamad <aghi.mohamad@gmail.com>
2022-09-12 14:25:26 +0300

    projects added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 function Project({project}) {
   return (
-    <div className='flex flex-col md:flex-row justify-around p-8 items-center'>
+    <div className='flex flex-col md:flex-row justify-around my-12 items-center'>
         <div className='md:w-[45%]'>
             <h1 className='text-xl font-bold mb-7'>{project.title}</h1> 
             <div className='flex flex-wrap'>
@@ -18,7 +18,7 @@ function Project({project}) {
             <p className='text-[#666] leading-relaxed mb-7'>{project.description}</p>
         </div>
         <div className='relative group md:w-[45%] rounded-3xl overflow-hidden hover:-translate-y-5 hover:opacity-90 cursor-pointer  transition-all duration-200 w-64'>
-                    <img src={project.image1} alt="" className='  transition-all duration-200 hover:blur-sm  '/>
+                    <img src={project.image} alt="" className='  transition-all duration-200 hover:blur-sm  '/>
                     <a href={project.live} className="absolute opacity-0 group-hover:opacity-100 hover:block top-[50%] left-[50%] rounded-xl bg-[#facf0f] px-6 py-2 text-2xl md:font-bold -translate-y-[50%] -translate-x-[50%] z-11 transition-all duration-200">Visit It!</a>
         </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,17 +16,35 @@ function Projects() {
             description : `LetsShare is a website that allow people who have extra belongings make donations and provide a sharing space for people in need for requesting the goods they need.  The aim of the website is to encourage people who live in the same region to share their daily extra food, clothes, furniture and more! The website is intended for refugees living in Turkey, especially in refugee camps. On this website, you can find various types of goods that are ready for a donation, and also requests. Types of goods are categorized as clothing, furniture,  electronics and much more. `,
             github : "https://github.com/ReCoded-Org/lets-share-capstone-project",
             live : "https://lets-share-proj.netlify.app/",
-            image1 : "/letsshare.png",
-            image2 :  "/letsshare2.jpeg"
-        }
+            image : "/letsshare.png",
+                },
+        {
+            title : "movie app",
+            stack : ["HTML", "CSS", "JavaScript",  "Git", "GitHub", "Responsive Design", "API"],
+            description : "this app is for viewing movies and actors, we used an external API called \" movie dp\" and we used plain javaScript to make this happend there's a page for each category also you can filter using top watched and recent movies, you can also search movies by title, take a look at it! ",
+            live : "https://recoded-bprm-turkey-few-2022.github.io/movie-project-bamya/",
+            github : "https://github.com/Recoded-BPRM-Turkey-FEW-2022/movie-project-bamya",
+            image : "/movie.png"
+        },
+        {
+            title : "Facebook clone",
+            stack : ["Git", "GitHub", "Responsive Design","Reactjs", "Nextjs", "NextAUTH", "Git", "GitHub", "Responsive Design", "TailwindCSS", "Firebase",],
+            description : "this app is something that looks like facebook, i built it to learn authentication using NextAUTH and dealing with database using firebase the way authentication is handled is using facebook account through NextAUTH and you can submit posts that will be saved and fetched from the firebase database using firestore, ",
+            live : "https://facebook-clone-three-ruddy.vercel.app/",
+            github : "https://github.com/aghimohamad/facebood-clone",
+            image : "/facebook.png"
+        },
+
     ]
 
 
 
   return (
-    <div>
+    <div className='p-8'>
         <Heading heading={heading} />
-        <Project project={projects[0]} />
+        {projects.map(project => (
+            <Project key={project.title} project={project} />
+        ))}
     </div>
   )
 }
